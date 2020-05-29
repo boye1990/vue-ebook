4eb2412e76b55392ed41389be95ce949773199b3
boye <boye_1990@aliyun.com>
2020-05-29 15:29:14 +0800

    添加global和resetscss以及设置rem布局

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,18 @@
     <router-view/>
   </div>
 </template>
+<script>
+export default {}
+document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html')
+  let fontSize = window.innerWidth / 10
+  fontSize = fontSize > 50 ? 50 : fontSize
+  html.style.fontSize = fontSize + 'px'
+})
+</script>
 
-<style>
+<style lang="scss">
+@import './assets/styles/global.scss';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,6 +26,6 @@
 .fontStyle {
   font-family: 'Days One';
   color: orange;
-  font-size: 40px;
+  font-size: px2rem(20);
 }
 </style>
