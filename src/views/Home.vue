3911d812750dfce725983d8143f80fb1d49fc4be
boye <boye_1990@aliyun.com>
2020-05-29 10:13:39 +0800

    电子书渲染

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="home">
-    aaa
+    <div id="read"></div>
   </div>
 </template>
 
 <script>
+import Epub from 'epubjs'
+global.ePub = Epub
 export default {
-  name: 'Home'
+  name: 'Home',
+  data () {
+    return {
+      book: ''
+    }
+  },
+  mounted () {
+    this.book = new Epub('/2010_Book_CyborgsInLatinAmerica.epub')
+    this.book.renderTo('read', {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }).display()
+  }
 }
 </script>
