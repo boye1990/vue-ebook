586f7f893cf09ba075295393bc8054bdf5709650
boye <boye_1990@aliyun.com>
2020-06-02 00:03:33 +0800

    添加手势翻页

--- src/components/ebook/EbookReader.vue
+++ src/components/ebook/EbookReader.vue
@@ -20,6 +20,18 @@ export default {
     })
   },
   methods: {
+    // 去下一页的方法
+    prevPage () {
+      if (this.rendition) this.rendition.prev()
+    },
+    // 返回上一页的方法
+    nextPage () {
+      if (this.rendition) this.rendition.next()
+    },
+    // 显示头部和底部
+    toggleTitleAndMenu () {
+
+    },
     // 电子书初始化
     initEpub () {
       // 实例epub，传入获取的资源链接
@@ -31,6 +43,27 @@ export default {
         methods: 'default' // 微信兼容性配置
       })
       this.rendition.display()
+      // 监听touchstart 和 touchend 事件。
+      this.rendition.on('touchstart', event => {
+        // 获取触摸起点x坐标，和时间
+        this.touchstartX = event.changedTouches[0].clientX
+        this.touchstartTime = event.timeStamp
+      })
+      this.rendition.on('touchend', event => {
+        // 获取触摸终点x坐标，和时间
+        const offsetX = event.changedTouches[0].clientX - this.touchstartX
+        const time = event.changedTouches[0].clientX - this.touchstartTime
+        if (offsetX > 40 && time < 500) {
+          this.prevPage()
+        } else if (offsetX < -40 && time < 500) {
+          this.nextPage()
+        } else {
+          this.toggleTitleAndMenu()
+          console.log(offsetX, time)
+        }
+        // event.preventDefault()
+        event.stopPropagation()
+      })
     }
   }
 
