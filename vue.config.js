abcc364d4133a53b440087fcee9bcb65050216ea
boye <boye_1990@aliyun.com>
2020-06-07 21:06:34 +0800

    添加全局环境变量

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
-if (process.env.NODE_ENV === 'development') process.env.VUE_APP_RES_URL = 'http://192.168.1.4:8081'
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
 }
