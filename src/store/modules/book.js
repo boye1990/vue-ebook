6988c3c9801d42d935498fdaec90ed2306191e05
boye <boye_1990@aliyun.com>
2020-06-07 12:20:39 +0800

    将常量抽出

--- src/store/modules/book.js
+++ src/store/modules/book.js
@@ -1,3 +1,4 @@
+import { THEME_LIST } from '../../utils/book'
 const book = {
   state: {
     fileName: '',
@@ -7,51 +8,12 @@ const book = {
     isShow: false,
     // 默认主题
     defaultTheme: 'default',
+    // epub实例
+    epubBook: null,
     // 电子书加载状态
     bookAvailable: false,
     // 主题设置列表
-    themeList: [
-      {
-        name: 'default', // 主题名称
-        title: '默认',
-        style: {
-          body: {
-            color: '#000', // 主题字体颜色
-            background: '#7d8188' // 主题背景颜色
-          }
-        }
-      },
-      {
-        name: 'elegant', // 主题名称
-        title: '雅致',
-        style: {
-          body: {
-            color: '#000', // 主题字体颜色
-            background: 'rgb(198, 194, 182)' // 主题背景颜色
-          }
-        }
-      },
-      {
-        name: 'eye', // 主题名称
-        title: '护眼',
-        style: {
-          body: {
-            color: '#000', // 主题字体颜色
-            background: 'rgb(169, 193, 169)' // 主题背景颜色
-          }
-        }
-      },
-      {
-        name: 'night', // 主题名称
-        title: '夜间',
-        style: {
-          body: {
-            color: '#fff', // 主题字体颜色
-            background: '#000' // 主题背景颜色
-          }
-        }
-      }
-    ]
+    themeList: THEME_LIST
   },
   mutations: {
     SET_FILENAME: (state, fileName) => {
---
+++ src/utils/book.js
@@ -0,0 +1,43 @@
+
+export const THEME_LIST = [
+  {
+    name: 'default', // 主题名称
+    title: '默认',
+    style: {
+      body: {
+        color: '#000', // 主题字体颜色
+        background: '#7d8188' // 主题背景颜色
+      }
+    }
+  },
+  {
+    name: 'elegant', // 主题名称
+    title: '雅致',
+    style: {
+      body: {
+        color: '#000', // 主题字体颜色
+        background: 'rgb(198, 194, 182)' // 主题背景颜色
+      }
+    }
+  },
+  {
+    name: 'eye', // 主题名称
+    title: '护眼',
+    style: {
+      body: {
+        color: '#000', // 主题字体颜色
+        background: 'rgb(169, 193, 169)' // 主题背景颜色
+      }
+    }
+  },
+  {
+    name: 'night', // 主题名称
+    title: '夜间',
+    style: {
+      body: {
+        color: '#fff', // 主题字体颜色
+        background: '#000' // 主题背景颜色
+      }
+    }
+  }
+]
